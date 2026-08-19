c634e14b09e512605e4a1d9f094ea94831996c35
errnic3-creator <errnic3@gmail.com>
2026-08-20 00:46:09 +0800

    Update ExampleRobolectricTest.kt

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -19,7 +19,7 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("ScreenTime Lock", appName)
+    assertEquals("ScrMngr", appName)
   }
 
   @Test
